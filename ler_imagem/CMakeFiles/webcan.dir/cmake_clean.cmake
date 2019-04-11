bdaff23c3bb7f42b69d39c75b05e3dda7e00514c
Rafael Ramos <ramosunb@gmail.com>
2019-04-10 22:13:48 -0300

    código que ler imagem

---
+++ ler_imagem/CMakeFiles/webcan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webcan.dir/webcan.cpp.o"
+  "webcan.pdb"
+  "webcan"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webcan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ler_imagem/CMakeFiles/webcan.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for webcan.
+# This may be replaced when dependencies are built.
